5a539f4359b65957091234a05c0954ddbca12cc6
jamesnulliu <jamesnulliu@gmail.com>
2025-11-09 12:22:22 +0000

    [update]

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -4865,6 +4865,7 @@ class Trainer:
             tuple[Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor]]: A tuple with the loss,
             logits and labels (each being optional).
         """
+        print(f"transformers.trainer.Trainer.prediction_step: input keys: {inputs.keys()}")
         has_labels = False if len(self.label_names) == 0 else all(inputs.get(k) is not None for k in self.label_names)
         # For CLIP-like models capable of returning loss values.
         # If `return_loss` is not specified or being `None` in `inputs`, we check if the default value of `return_loss`
